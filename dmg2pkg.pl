7daeef2b3f50cf9656f6ba02634de806e8c65670
Jason Campisi <aitsinformation@gmail.com>
2021-04-06 18:02:42 -0400

    nuts and bolts
    harvest app data id and version number flag mode check has been added and a verbose mode.
    
    this is precursor to either either including that code to harvest or just call the other apps to do that part.

--- dmg2pkg.pl
+++ dmg2pkg.pl
@@ -10,28 +10,31 @@
 use strict;
 use Getopt::Long;
 
-my ($volume, $ver, $id, $save, $help, $volName, $verbose) = ("","","","",0, "", 0);
+my ($volume, $ver, $id, $save, $help, $volName, $verbose, $harvest) = ("","","","",0, "", 0,0);
 my $vol="/Volumes/";
-GetOptions( "n:s" =>\$volume, "v:s" =>\$ver, "i:s" =>$id,
-            "s:s"  =>\$save,  "help" =>\$help, "verbose" =>\$verbose) or usage();
+GetOptions( "n:s" =>\$volume, "v:s" =>\$ver,    "i:s" =>$id,
+            "s:s"  =>\$save,  "help" =>\$help,  "verbose" =>\$verbose,
+            "a" =>\$harvest) or check();
             
 sub usage(){ # check required data or if help was called
 
   print <<EOD;
-dmg2pkg.pl Converts mounted dmg install folders and convert them to a pkg installer package for MDM
-    deployment. 
+dmg2pkg.pl Converts mounted dmg install folders and convert them to a pkg installer 
+    package for MDM deployment. 
 
     Ussage:         dmg2pkg.pl -n VolumeNAME -v appVersion -s -i appBundleIdentifier path-to-save-Mac
             
                     -n Name of mounted DMG Volume
-                    -v version The application encoded version number. Mke sure to sync this with the version you are 
+                    -v Version The application encoded version number. Mke sure to sync this with the version you are 
                         trying to deploy (if it’s VLClan v3.1.12, then this parameter is 3.1.12). How to find 
                         the encoded version number go to the section Extra Detail for more information
-                    -i application bundle identifier. Go to the secion Extra Detail for more information
-                    -s is the locaiton & name of the installer pkg file you want to save to
+                    -i Application bundle identifier. Go to the secion Extra Detail for more information
+                    -s This is the locaiton & name of the installer pkg file you want to save to
                     
     Optional        -help
                     -verbose
+                    -a AppData...gather the required app version number and bundle id info automaticly.
+                        Requires the app to be installed in /Applications/ folder
 
     Requirement:    Must have a dmg file you have opened/mounted for this program to work
     
@@ -54,17 +57,39 @@ EOD
 }#end usage
 
 sub check(){
+
+#temp debug code
+    if ($verbose){
+        print "temp debug code:\n";
+        print "  volume location = $volume\n";
+        print "  harvest appData = $harvest\n";
+        print "  id = $id\n";
+        print "  version = $ver\n";
+        print "  save location = $save\n";
+        print "  help = $help\n\n";
+    }
+#end temp debug code
+
    usage() if $help;
    usage() if ($volume eq ""); #check if Volume exists and format info
     $volume=~s/\\//g;  # remove forward slashs
-    $volume=$vol . $volume if (not $volume =~ m/^$vol/); #add /Volumes if not there
+    $volume=$vol . $volume if (not $volume =~ m/^$vol/); #add /volumes/ if not there so "foo" becomes /volumes/foo
     $volName=$1 if ($volume =~ m/^$vol(.*)\/$/); #harvest app name
     print "Program Name: $volName\n" if $verbose;
     usage() if (not -d $volume); #does volume exist?
     print "DMG Mounted Volume found: $volume\n" if $verbose;
     
     #check the rest of the data if it is present to work with
-    usage() if (($save eq "") and ($ver eq "") and ($id eq ""));
+   if ($harvest){
+       #gather app bundle ID and appversion number
+       print "Harvest mode\n" if $verbose;
+       #ADD CODE HERE
+        
+   }else{
+       usage() if ($ver eq "") and ($id eq "");
+    
+   }
+   usage() if ($save eq "");
 
 }#end check
 
